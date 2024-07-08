9fd8abea3f68446658030d51dc768c1699b609c4
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-07-08 12:19:05 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+import { Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+/* import App.style.js file here */
+import { s } from "./App.style";
+export default function App(){
+  return(
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={s.app}>
+          <View style={s.header}>
+            <Text>Header</Text>
+          </View>
+          <View style={s.body}>
+            <Text>Body</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={s.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ App.style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+    app: {
+        flex: 1,
+        backgroundColor: "#F9F9F9",
+
+    },
+    header: {
+        flex: 1,
+    },
+    body: {
+        flex: 5,
+    },
+    footer: {
+        height: 70,
+    },
+});
